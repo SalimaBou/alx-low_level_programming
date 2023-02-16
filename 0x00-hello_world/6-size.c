4b3adb21ee5dda22bad4c55a44411f813434ee60
SalimaBou <sal.bouhamidi@gmail.com>
2023-02-16 13:08:06 -0800

    a

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,8 +2,6 @@
 /**
  * main - sizeof
  *
- * Description: 'in this task we will use sizeof function'
- *
  * Return: 0
  */
 int main(void)
@@ -12,6 +10,6 @@ int main(void)
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %d byte(s)", sizeof(float));
-return (0);
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
 }
