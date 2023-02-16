35ed5470c38653c5efb6c00fc04e96f627ae8fa5
SalimaBou <sal.bouhamidi@gmail.com>
2023-02-16 12:27:19 -0800

    h

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio>
+/**
+ * main - sizeof
+ *
+ * Description: 'in this task we will use sizeof function'
+ *
+ * Return: 0
+ */
+int main(void)
+{
+printf("\"%d\n", "%d\n", "%d\n", "%d\n", "%d\n", sizeof(char), sizeof(int), sizeof(long - int), sizeof(long - long - int), sizeof(float)");
+return (0)
+}
