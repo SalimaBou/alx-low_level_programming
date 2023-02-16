8bf12f9491f3570e5a3746fd416661a1caadf5fd
SalimaBou <sal.bouhamidi@gmail.com>
2023-02-16 12:47:16 -0800

    let's practice with betty

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,6 +8,10 @@
  */
 int main(void)
 {
-printf("\"%d\n", "%d\n", "%d\n", "%d\n", "%d\n", sizeof(char), sizeof(int), sizeof(long - int), sizeof(long - long - int), sizeof(float)");
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+printf("Size of a float: %d byte(s)", sizeof(float));
 return (0)
 }
