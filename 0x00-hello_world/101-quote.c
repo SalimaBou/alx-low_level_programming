89f64d3bad4212131bede48b16039d39481ef88c
SalimaBou <sal.bouhamidi@gmail.com>
2023-02-16 14:19:42 -0800

    a

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 /**
-   * main - challenging task
-   *
-   * description: as you can see this is print command
-   *
-   * Return: one
-   */
+ * main - main block
+ * Return: 1
+ */
 int main(void)
 {
-	printf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (1);
+        int s;
+
+        s = strlen("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+
+        write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", s);
+        return (1);
 }
