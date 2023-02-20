3010a01af8b357c44009221b10c4903c46e9dd04
SalimaBou <sal.bouhamidi@gmail.com>
2023-02-20 01:24:04 -0800

    digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - this program shows digit numbers base 10
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int s;
+	{
+	for (s = '0'; s <= '9'; s++)
+		putchar(s);
+	}
+	putchar('\n');
+	return (0);
+}
