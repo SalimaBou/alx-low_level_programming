4abe59c02f7e5253fdde38b0079b9227eb1d6cba
SalimaBou <sal.bouhamidi@gmail.com>
2023-02-20 10:14:26 -0800

    a

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - main
+ *
+ * Return:0
+ */
+int main(void)
+{
+int s;
+
+for (s = '0'; s <= '9'; s++)
+{
+putchar(s);
+if (s < 9)
+{
+putchar(',');
+putchar(' ');
+}}
+putchar('\n');
+return (0);
+}
