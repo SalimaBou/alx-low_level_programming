b3081ba0fbacfa4f6e529cf944f98f98008cc500
SalimaBou <sal.bouhamidi@gmail.com>
2023-02-21 13:12:16 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isalpha -_isalpha will detect if the char is from alphabet or not
+ * @c : character
+ * Return: 0 || 1
+ */
+int _isalpha(int c)
+{
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+{
+return (1);
+}
+else
+{
+return (1);
+}
+}
