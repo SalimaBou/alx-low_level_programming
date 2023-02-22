b33a24265f66e55c4545fdd204debbf6fcc8b93f
SalimaBou <sal.bouhamidi@gmail.com>
2023-02-22 02:59:09 -0800

    absolute numbers

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - it cverts any number to an absolute value.
+ * @n: the character
+ * Return: n or -n
+ */
+int _abs(int n)
+{
+if (n >= 0)
+return (n);
+else
+return (-n);
+}
