7740afb5f926771e52cb6d0e0052cfa6fcc0b76e
SalimaBou <sal.bouhamidi@gmail.com>
2023-02-23 05:17:03 -0800

    print  numbers without return

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - to print digit numbers from 0 to 9
+ */
+void print_numbers(void)
+{
+int s;
+for (s = '0'; s <= '9'; s++)
+{
+_putchar(s);
+}
+_putchar('\n');
+}
