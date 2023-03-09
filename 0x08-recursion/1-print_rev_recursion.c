bd04df3411bceb36d149f72c5106910a5a95d55b
SalimaBou <sal.bouhamidi@gmail.com>
2023-03-09 12:51:42 -0800

    hay hay hay

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,14 +2,13 @@
 /**
  * _print_rev_recursion - print a string in reverse
  * @s: string
- * Return 0;
-*/
+ */
 
 void _print_rev_recursion(char *s)
 {
-	if (*s)
-	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
-	}
+	if (*s == '\0')
+		return;
+
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
